84bd43564c559256168e15ff1d827ecacf941234
Thales Ricardo Menezes <thalesmenezes286@gmail.com>
2024-01-29 10:55:07 -0300

    Adicionando e atualizando elementos no Array

--- arrays-js/adicionando-nota.js
+++ arrays-js/adicionando-nota.js
@@ -1,7 +1,7 @@
 
 const notas = [10,6,8];
 
-//Adiciona uma nota nota ao array
+//Adiciona um elemento no final do Array
 notas.push(7);
 
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
---
+++ arrays-js/atualizando-lista.js
@@ -0,0 +1,7 @@
+
+const nomes = ['João','Ana','Caio','Lara','Marjorie','Leo'];
+
+//remover a Ana e o Caio e add Rodrigo
+nomes.splice(1,2,"Rodrigo");
+
+console.log(nomes);
