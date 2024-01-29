28b2b9f013c82c688f82c1a71b17344e6f431f1a
Thales Ricardo Menezes <thalesmenezes286@gmail.com>
2024-01-29 11:17:49 -0300

    Lista de duas dimensões

---
+++ arrays-js/juntando-salas.js
@@ -0,0 +1,7 @@
+
+const salaJS = ["Evandro","Camis","Mari"];
+const salaPython = ["Ju","Leo","Raquel"];
+
+const salasUnificadas = salaJS.concat(salaPython);
+
+console.log(salasUnificadas);
---
+++ arrays-js/lista-duas-dimensoes.js
@@ -0,0 +1,6 @@
+
+const alunos = ['João','Juliana','Ana','Caio'];
+const media = [10,8,7.5,9];
+
+const listaMediaDeAluno = [alunos, media];
+console.log(`A aluna da posição 1 da lista de alunos é: ${listaMediaDeAluno[0][1]}, a nota dessa aluna é ${listaMediaDeAluno[1][1]}`)
