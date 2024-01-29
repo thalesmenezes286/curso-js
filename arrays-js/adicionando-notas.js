11ec6ee1f8c21b7fe545c04dd6fd812d12c183ce
Thales Ricardo Menezes <thalesmenezes286@gmail.com>
2024-01-29 00:13:16 -0300

    Adicionando elementos no Array

---
+++ arrays-js/adicionando-notas.js
@@ -0,0 +1,9 @@
+
+const notas = [10,6,8];
+
+//Adiciona uma nota nota ao array
+notas.push(7);
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log("Média: ",media);
